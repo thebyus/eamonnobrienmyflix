a5d2d080be2428f3dca5242445bbcab4df780ab4
Eamonn O'Brien <thebyusgames@gmail.com>
2013-08-07 22:27:38 -0700

    video has many categories test success
    kind of a weak method (measuring size, instead of verifying data
    applied)

--- spec/Models/video_spec.rb
+++ spec/Models/video_spec.rb
@@ -13,10 +13,11 @@ describe Video do
 end
 
 describe Video do
-	it "belongs to a category" do
-		Category.create!(cat:"Test")
+	it "can belong to many categories" do
+		cat1 = Category.create!(cat:"Test")
+		cat2 = Category.create!(cat: "Test2")
 		vid = Video.create!(title: "Test2")
-		vid.categories << Category.first
-		vid.categories.should =="Test"
+		vid.categories << [cat1, cat2]
+		vid.categories.size.should == 2
 	end
 end
