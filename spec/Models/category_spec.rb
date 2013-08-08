e614bfd89ae3d224e870950e8967ea92102abc03
Eamonn O'Brien <thebyusgames@gmail.com>
2013-08-08 13:57:29 -0700

    testing video validations work
    validations for title and description work

--- spec/Models/category_spec.rb
+++ spec/Models/category_spec.rb
@@ -11,9 +11,9 @@ end
 
 describe Category do 
 	it "has many videos"	 do
-		vid1 = Video.create!(title: "Test1")
-		vid2 = Video.create!(title: "Test2")
-		vid3 = Video.create!(title: "Test3")
+		vid1 = Video.create!(title: "Test1", description: "Test1")
+		vid2 = Video.create!(title: "Test2", description: "Test2")
+		vid3 = Video.create!(title: "Test3", description: "Test3")
 		cat1 = Category.create!(cat:"Test1")
 		
 		vid1.categories << [cat1]
--- spec/Models/video_spec.rb
+++ spec/Models/video_spec.rb
@@ -16,10 +16,25 @@ describe Video do
 	it "can belong to many categories" do
 		cat1 = Category.create!(cat:"Test")
 		cat2 = Category.create!(cat: "Test2")
-		vid = Video.create!(title: "Test2")
+		vid = Video.create!(title: "Test2", description: "Test")
 
 		vid.categories << [cat1, cat2]
-		
+
 		vid.categories.should == [cat1, cat2]
+	end
+end
+
+
+describe Video do
+	it "validates title" do
+		vid = Video.new(description: "Bad Movie!", title:"")
+		vid.save.should == false
+	end
+end
+
+describe Video do
+	it "validates description" do
+		vid = Video.new(title: "Bad Movie", description:"")
+		vid.save.should == false
 	end
 end
