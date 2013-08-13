de52b0d75574b2896262583cf339ba8f8463f15d
Eamonn O'Brien <thebyusgames@gmail.com>
2013-08-13 13:32:23 -0700

    user controller passes tests

--- spec/Controllers/users_spec.rb
+++ spec/Controllers/users_spec.rb
@@ -7,29 +7,41 @@ describe UsersController do
       expect(assigns(:user)).to be_instance_of(User)
     end
   end
+
   describe "POST create" do
+
     context "with valid input" do
+
+      before do
+        post :create, user: Fabricate.attributes_for(:user)
+      end
+
       it "creates user" do
-#        tom = Fabricate(:user)
-        post :create, user: { email: "joe@junk.com", password: "password", full_name: "Joe Blow"}
-#        post :create, user: (Fabricate(:user)) 
         expect(User.count).to eq(1)
       end
+
       it "redirects to the sign in page" do
-        post :create, user: { email: "joe@junk.com", password: "password", full_name: "Joe Blow"}
         expect(response).to redirect_to home_path
       end
     end
+
     context "with invalid input" do
+
+      before do
+       post :create, user: { password: "password", full_name: "Joe Blow"}
+      end
+
       it "does not create user" do
-        post :create, user: { password: "password", full_name: "Joe Blow"}
         expect(User.count).to eq(0)
       end
+
       it "renders new template view" do
-        post :create, user: { password: "password", full_name: "Joe Blow"}
         expect(response).to render_template :new
       end
-      it "sets @user variable"
+
+      it "sets @user variable" do
+        expect(assigns(:user)).to be_instance_of(User)
+      end
     end
   end
 end
