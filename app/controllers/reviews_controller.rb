808a164e2b6374360a10c4b5479cee291181f37c
Eamonn <thebyusgames@gmail.com>
2013-08-16 14:20:24 -0700

    review controller testing

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,7 @@
 class ReviewsController<ApplicationController
   def create
-
+    video = Video.find(params[:video_id])
+    video.reviews.create(params.require(:review).permit(:rating, :content))
+    redirect_to video
   end
 end
--- spec/Controllers/reviews_controller_spec.rb
+++ spec/Controllers/reviews_controller_spec.rb
@@ -4,14 +4,25 @@ describe ReviewsController do
   describe "POST create" do
     context "with authenticated users" do
       context "valid input" do
+        it "redirects to video show page" do
+          video = Fabricate(:video)
+          post :create, review: Fabricate.attributes_for(:review), video_id: video.id
+          expect(response).to redirect_to video
+        end
+
         it "creates a review" do
           video = Fabricate(:video)
-          post :create, review: Fabricate.attributes_for(:review, video_id: video.id)
+          post :create, review: Fabricate.attributes_for(:review), video_id: video.id
           expect(Review.count).to eq(1)
         end
-        it "creates a review associated with video"
+
+        it "creates a review associated with video" do
+          video = Fabricate(:video)
+          post :create, review: Fabricate.attributes_for(:review), video_id: video.id
+          expect(Review.first.video).to eq(video)
+        end
         it "creates a review associated with signed in user"
-        it "redirects to video show page"
+
       end
       context "with invalid input"
     end
