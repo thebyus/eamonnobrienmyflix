aa7169e5ebf5c62251ecb5fbae6c355cce5c1728
Eamonn <thebyusgames@gmail.com>
2013-08-19 08:46:45 -0700

    first queue test passed

--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -1,3 +1,3 @@
 class QueueItem <ActiveRecord::Base
-
+    belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User <ActiveRecord::Base
-	validates_presence_of :email, :full_name, :password
-	validates_uniqueness_of :email
+  validates_presence_of :email, :full_name, :password
+  validates_uniqueness_of :email
 
-	has_secure_password	validations: false
+  has_secure_password validations: false
+  has_many :queue_items
 end
