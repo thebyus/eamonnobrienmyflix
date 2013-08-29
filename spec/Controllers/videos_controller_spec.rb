23148f35f99ede499aa5654f15cda855e60e48f0
Eamonn <thebyusgames@gmail.com>
2013-08-29 12:06:33 -0700

    errors

--- spec/Controllers/videos_controller_spec.rb
+++ spec/Controllers/videos_controller_spec.rb
@@ -12,10 +12,10 @@ describe VideosController do
     it "sets the @review variable for authenticated users" do
       session[:user_id] = Fabricate(:user).id
       vid = Fabricate(:video)
-      review1 = Fabricate(:review, video:vid)
-      review2 = Fabricate(:review, video:vid)
+      review1 = Fabricate(:review, video: vid)
+      review2 = Fabricate(:review, video: vid)
       get :show, id: vid.id
-      expect(assigns(:reviews)).to match_array ([review1, review2])
+      expect(assigns(:reviews)).to match_array( [review1, review2] )
     end
 
     it "redirects to sign_in for unauthenticated users" do
@@ -24,6 +24,7 @@ describe VideosController do
       expect(response).to redirect_to sign_in_path
     end
   end
+
   describe "POST search" do
     it "sets @search_result variable for authenticated user"do
       session[:user_id] = Fabricate(:user).id
@@ -32,6 +33,7 @@ describe VideosController do
       post :search, search_term: 'ar'
       expect(assigns(:search_result)).to eq([argo])
     end
+
     it "redirects unauthenticated user to sign_in" do
       argo = Fabricate(:video, title: "Argo")
       flight = Fabricate(:video, title: "Flight")
