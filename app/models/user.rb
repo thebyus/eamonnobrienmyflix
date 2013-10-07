54e4bf5de158328a2a5c5147a01846d690e79339
Eamonn <thebyusgames@gmail.com>
2013-10-06 22:09:26 -0700

    concerns created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User <ActiveRecord::Base
+  include Tokenable
+
   validates_presence_of :email, :full_name, :password
   validates_uniqueness_of :email
 
@@ -7,8 +9,6 @@ class User <ActiveRecord::Base
   has_many :reviews, order: "created_at DESC"
   has_many :following_relationships, class_name: "Relationship",foreign_key: :follower_id
   has_many :leading_relationships, class_name: "Relationship",foreign_key: :leader_id
-
-  before_create :generate_token
 
   def normalize_queue_item_positions
     queue_items.each_with_index do |queue_item, index|
@@ -34,9 +34,5 @@ class User <ActiveRecord::Base
 
   def to_param
     token
-  end
-
-  def generate_token
-    self.token = SecureRandom.urlsafe_base64
   end
 end
