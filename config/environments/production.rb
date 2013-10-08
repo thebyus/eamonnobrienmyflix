1347abd29ddaaa9d85f5af3ff5cdb99c1eea4deb
Eamonn <thebyusgames@gmail.com>
2013-10-07 20:59:31 -0700

    test11

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,15 +69,16 @@ Myflix::Application.configure do
 
   config.eager_load = true
 
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'eamonnobrienmyflix.heroku.com',
-    :authentication => :plain, }
+    :domain         => 'eamonnobrienmyflix',
+    :authentication => :plain,
+    }
 
-  ActionMailer::Base.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
 
 =begin
   config.action_mailer.smtp_settings = {
