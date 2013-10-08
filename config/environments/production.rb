910fe3ad36b1b2311ec1704bad37c1862b5ad24e
Eamonn <thebyusgames@gmail.com>
2013-10-07 21:08:28 -0700

    test12

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,9 +74,8 @@ Myflix::Application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'eamonnobrienmyflix',
-    :authentication => :plain,
-    }
+    :domain         => 'eamonnobrienmyflix.heroku.com',
+    :authentication => :plain, }
 
   config.action_mailer.delivery_method = :smtp
 
