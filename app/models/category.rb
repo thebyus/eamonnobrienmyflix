b362c98e64783dbd63a185ef9b1381a57b2116b3
Eamonn <thebyusgames@gmail.com>
2014-01-25 16:50:59 -0800

    added titles and open in new link

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category <ActiveRecord::Base
-	has_many :video_categories
-	has_many :videos, through: :video_categories, order: "created_at DESC"
+  has_many :video_categories
+  has_many :videos, through: :video_categories, order: "created_at DESC"
   validates_presence_of :cat
 
   def recent_videos
